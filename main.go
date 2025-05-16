7e665ed0a76b05dbeb0dcc2f7fed8f92205cfdd0
Can <c.c@c.com>
2025-05-16 09:10:54 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("letters.txt")
+	if err != nil {
+		panic(err)
+	}
+	n, err := countAlphabets(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Letters: %d\n", n)
+}
+
+func countAlphabets(r io.Reader) (int, error) {
+	count := 0
+	buffer := make([]byte, 1024)
+
+	for {
+		n, err := r.Read(buffer)       // infinite loop, read 1024 chars at a time
+		for _, l := range buffer[:n] { //loop on the n chars read
+			if (l >= 'A' && l <= 'Z') || (l >= 'a' && l <= 'z') {
+				count++
+			}
+		}
+		if err == io.EOF {
+			return count, nil
+		}
+		if err != nil {
+			return 0, err
+		}
+	}
+}
